d61f4eb2990c741e27090da74d0fd373d53fd364
WIIASD <66344652+WIIASD@users.noreply.github.com>
2021-02-23 13:42:03 -0500

    Restructure both classes

--- Force Based 2D Controller/Assets/Scripts/Controller2D.cs
+++ Force Based 2D Controller/Assets/Scripts/Controller2D.cs
@@ -11,15 +11,18 @@ using UnityEngine;
 /// </summary>
 public class Controller2D : MonoBehaviour
 {
-    Player player;
-    public bool jumpPressed, jumpReleased;
     public float speedModifier = 3f;
     public float jumpStrength = 15f;
     public float basicJumpStrength = 3f;
     public float timeToReachJumpApex = 0.2f;
-    float jumpW;
-    int jumpBoost = 0;
-    float horaxis = 0f;
+    public bool isJumping = false;
+    public bool apexReached = false;
+    public int wallJumpDirection;
+
+    private int jumpBoost = 0;
+    private float jumpW;
+    private Player player;
+    
     BoxCollider2D boxCollider;
     List<Collider2D> colliderStandedOn = new List<Collider2D>();
     List<Collider2D> colliderWalled = new List<Collider2D>();
@@ -32,39 +35,62 @@ public class Controller2D : MonoBehaviour
         player = GetComponent<Player>();
         jumpW = (Mathf.PI / 2) / (timeToReachJumpApex / 0.02f); // Calculate jumpW based on timeToReachApex
     }
-
     private void Update()
     {
-        updateInput();
-        updatePlayerInfo();
     }
     private void FixedUpdate()
     {
-        move();
-        jump();
+        HandleMove();
+        HandleJump();
     }
+
+    public bool IsGrounded(){
+        return colliderStandedOn.Count < 1 ? false : true;
+    }
+
+    public bool IsWalled(){
+        return colliderWalled.Count < 1 ? false : true;
+    }
+
+    public void Jump(){
+        
+        if(!IsGrounded()){//before jump
+            return;
+        }
+
+        rb2d.velocity = new Vector2(rb2d.velocity.x, basicJumpStrength);
+        isJumping = true;
+    }
+
+    public void StopJump(){
+        isJumping = false;
+        apexReached = false;
+        jumpBoost = 0;
+    }
+
     /// <summary>
-    /// move the player by adding a force every Fixed Update
+    /// move the player by adding a force every FixedUpdate frames
     /// </summary>
-    void move()
+    void HandleMove()
     {
-        Vector2 force = new Vector2(speedModifier * horaxis * rb2d.mass, 0);
+        Vector2 force = new Vector2(speedModifier * player.horaxis * rb2d.mass, 0);
         rb2d.AddForce(force);
     }
     /// <summary>
-    /// Jump by adding a force every Fixed Update
-    /// </summary>
-    void jump()
+    /// Jump by adding a force every FixedUpdate frames
+    /// /// </summary>
+    void HandleJump()
     {
-        if (player.isJumping)
+        if (isJumping)
         {
-            //this is an counter that keep track of the time in air
+            //this is an counter that keep track of the time in air in every FixedUpdate frames (0.02s)
             //will be reset after landing
             jumpBoost++;
             float C = Mathf.Cos(jumpW * jumpBoost);
             if (C < 0f)
             {
-                player.apexReached = true;
+                apexReached = true;
+                StopJump();
             }
             else
             {
@@ -72,53 +98,11 @@ public class Controller2D : MonoBehaviour
             }
         }
     }
-    /// <summary>
-    /// Update input from player every frame
-    /// </summary>
-    void updateInput()
-    {
-        horaxis = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            jumpReleased = false;
-            jumpPressed = true;
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            jumpPressed = false;
-            jumpReleased = true;
-        }
-    }
-    /// <summary>
-    /// Update player status information every frame
-    /// </summary>
-    void updatePlayerInfo()
-    {
-        player.grounded = colliderStandedOn.Count < 1 ? false : true;
 
-        player.walled = colliderWalled.Count < 1 ? false : true;
-
-        if (jumpPressed && player.grounded)
-        {
-            player.grounded = false;
-            player.isJumping = true;
-            jumpPressed = false;
-            rb2d.velocity = new Vector2(rb2d.velocity.x, basicJumpStrength);
-        }
-        if (jumpReleased || player.apexReached)
-        {
-            player.isJumping = false;
-            player.apexReached = false;
-            jumpBoost = 0;
-        }
-    }
     /// <summary>
-    /// The following 3 methods handle collision to ground with different normal
-    /// so that the player knows what kind of wall(s)/ground(s) 
-    /// is colliding.
-    /// I used arrays to store different gameobjects that
-    /// are colliding with the player
+    /// The following 3 methods handle collision to ground with different normal so that the player knows what kind of wall(s)/ground(s) is colliding.
+    /// I used arrays to store different gameobjects that are colliding with the player
     /// </summary>
     /// <param name="other"></param>
     private void OnCollisionEnter2D(Collision2D other)
@@ -134,14 +118,14 @@ public class Controller2D : MonoBehaviour
         {
             contactPoint.collider.GetComponent<SpriteRenderer>().color = Color.blue;
             colliderWalled.Add(contactPoint.collider);
-            player.wallJumpDirection = 1;
+            wallJumpDirection = 1;
         }
 
         if (contactPoint.normal.x < -0.8f && other.collider.CompareTag("Ground"))
         {
             contactPoint.collider.GetComponent<SpriteRenderer>().color = Color.blue;
             colliderWalled.Add(contactPoint.collider);
-            player.wallJumpDirection = -1;
+            wallJumpDirection = -1;
         }
     }
 
@@ -161,7 +145,7 @@ public class Controller2D : MonoBehaviour
             {
                 other.collider.GetComponent<SpriteRenderer>().color = Color.white;
                 colliderWalled.Remove(other.collider);
-                player.wallJumpDirection = 0;
+                wallJumpDirection = 0;
             }
         }
     }
--- Force Based 2D Controller/Assets/Scripts/Player.cs
+++ Force Based 2D Controller/Assets/Scripts/Player.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public bool grounded;
-    public bool walled;
+    private Controller2D controller2D;
+    public bool jumpPressed {get; private set;}
+    public bool jumpReleased {get; private set;}
+    public float horaxis{get; private set;}
 
-    public int wallJumpDirection;
-    public bool isJumping = false;
-    public bool apexReached = false;
-
+    private void Start() {
+        controller2D = GetComponent<Controller2D>();
+    }
     private void Update()
     {
-
+        updateInput();
     }
+
+    /// <summary>
+    /// Update input from player every frame
+    /// </summary>
+    void updateInput()
+    {
+        horaxis = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            controller2D.Jump();
+            jumpPressed = true;
+            jumpReleased = false;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            controller2D.StopJump();
+            jumpPressed = false;
+            jumpReleased = true;
+        }
+    }
+
 }
